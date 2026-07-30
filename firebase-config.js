4ea6a6215a1296555d48bf46040d4430120252c5
saikia6901-hub <saikia6901@gmail.com>
2026-07-31 02:56:23 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,24 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCy5M42kuuyOrhXA7hLglchYrgqEj4KNTQ",
-  authDomain: "anvi-travels-78c33.firebaseapp.com",
-  projectId: "anvi-travels-78c33",
-  storageBucket: "anvi-travels-78c33.firebasestorage.app",
-  messagingSenderId: "954247687836",
-  appId: "1:954247687836:web:b778008d73275646a80759",
-  measurementId: "G-H9VMQK95V4"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCy5M42kuuyOrhXA7hLglchYrgqEj4KNTQ",
+    authDomain: "anvi-travels-78c33.firebaseapp.com",
+    databaseURL: "https://anvi-travels-78c33-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "anvi-travels-78c33",
+    storageBucket: "anvi-travels-78c33.firebasestorage.app",
+    messagingSenderId: "954247687836",
+    appId: "1:954247687836:web:b778008d73275646a80759",
+    measurementId: "G-H9VMQK95V4"
+  };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export { app };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
